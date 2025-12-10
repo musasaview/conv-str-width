2bfc025c8ab042421511dd7643427a05502c5b25
musasaview <240800851+musasaview@users.noreply.github.com>
2025-12-11 07:16:04 +0900

    initial commit

--- index.test.ts
+++ index.test.ts
@@ -1,14 +1,26 @@
 import { expect, test } from "bun:test";
-import {toHalf, toFull} from './index';
+import { toHalf, toFull } from './index';
 
-test("全角→半角 英数字・記号", () => {
+const 半角カタカナ = "ｧｱｨｲｩｳｪｴｫｵｶｶﾞｷｷﾞｸｸﾞｹｹﾞｺｺﾞｻｻﾞｼｼﾞｽｽﾞｾｾﾞｿｿﾞﾀﾀﾞﾁﾁﾞｯﾂﾂﾞﾃﾃﾞﾄﾄﾞﾅﾆﾇﾈﾉﾊﾊﾞﾊﾟﾋﾋﾞﾋﾟﾌﾌﾞﾌﾟﾍﾍﾞﾍﾟﾎﾎﾞﾎﾟﾏﾐﾑﾒﾓｬﾔｭﾕｮﾖﾗﾘﾙﾚﾛヮﾜヰヱｦﾝｳﾞヵヶﾜﾞヸヹｦﾞ･ｰ､｡｢｣";
+const 全角カタカナ = "ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾタダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポマミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶヷヸヹヺ・ー、。「」";
+
+const 半角英数字 = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+const 全角英数字 = "！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～";
+
+test("英数字・記号", () => {
+    expect(toHalf(全角英数字)).toEqual(半角英数字);
+    expect(toFull(半角英数字)).toEqual(全角英数字);
+
+    // 変わらない
+    expect(toFull(全角英数字)).toEqual(全角英数字);
+    expect(toHalf(半角英数字)).toEqual(半角英数字);
 });
 
-test("全角→半角 カタカナ", () => {
-});
+test("カタカナ", () => {
+    expect(toHalf(全角カタカナ)).toEqual(半角カタカナ);
+    expect(toFull(半角カタカナ)).toEqual(全角カタカナ);
 
-test("半角→全角 英数字・記号", () => {
-});
-
-test("半角→全角 カタカナ", () => {
+    // 変わらない
+    expect(toFull(全角カタカナ)).toEqual(全角カタカナ);
+    expect(toHalf(半角カタカナ)).toEqual(半角カタカナ);
 });
